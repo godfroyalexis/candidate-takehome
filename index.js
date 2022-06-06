bf810e020677e8d484e3a54f753e838457d9f407
godfroyalexis <godfroy.alexis@gmail.com>
2022-06-06 16:31:28 +0200

    populate api route + search api route + theory assignments response

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
 const db = require('./models');
+
+const Op = db.Sequelize.Op;
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(express.static(`${__dirname}/static`));
 
+app.get('/api/games/populate', async (req, res) => {
+  try {
+    let os = {"android" : "https://interview-marketing-eng-dev.s3.eu-west-1.amazonaws.com/android.top100.json",
+                  "ios" : "https://interview-marketing-eng-dev.s3.eu-west-1.amazonaws.com/ios.top100.json"}
+    let options = {json: true};
+
+    for (let key in os) {
+      request(os[key], options, (error, response, body) => {
+        if (error) {
+          return  console.log(error)
+        }
+        if (!error && response.statusCode === 200) {
+            for (let gameArray in body) {
+              for (let game in body[gameArray]) {
+                let gameInfo = body[gameArray][game];
+                db.Game.create({
+                  publisherId : gameInfo.publisher_id,
+                  name : gameInfo.name,
+                  platform : gameInfo.os,
+                  bundleId: gameInfo.bundle_id,
+                  appVersion:gameInfo.version })
+              }
+            }
+        }
+      });
+    }
+    res.send("Database was populated");
+  } catch (err) {
+    return res.send(err);
+  }
+})
 
 app.get('/api/games', async (req, res) => {
   try {
@@ -16,6 +50,26 @@ app.get('/api/games', async (req, res) => {
     console.error('There was an error querying games', err);
     return res.send(err);
   }
+})
+
+app.post('/api/games/search', async (req, res) => {
+    try {
+     const games = await db.Game.findAll({
+       where: {
+         [Op.and]: [
+           req.body.platform ? { platform: req.body.platform } : "",
+           req.body.name ? { name: {
+               [Op.like]: "%" + req.body.name + "%"
+              }
+            } : "",
+         ]
+       }
+     })
+      return res.send(games);
+ } catch (err) {
+   console.error('There was an error querying games', err);
+   return res.send(err);
+ }
 })
 
 app.post('/api/games', async (req, res) => {
